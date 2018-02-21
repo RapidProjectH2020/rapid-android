b884365b6049e027709af4c013ae4e4474cd3875
Raffaele Montella <raffaele.montella@uniparthenope.it>
2018-02-21 16:16:59 +0100

    Added the performance evaluation for best provider automatic selection

--- rapid-gvirtus4a/src/main/java/eu/project/rapid/gvirtus4a/Providers.java
+++ rapid-gvirtus4a/src/main/java/eu/project/rapid/gvirtus4a/Providers.java
@@ -1,6 +1,9 @@
 package eu.project.rapid.gvirtus4a;
 
+import android.util.Log;
+
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.Vector;
 
 import eu.project.rapid.gvirtus4a.params.IntParam;
@@ -9,16 +12,19 @@ import eu.project.rapid.gvirtus4a.params.IntParam;
  * Created by raffaelemontella on 26/04/2017.
  */
 
-public class Providers {
+public class Providers  {
     private static final String LOG_TAG = "PROVIDERS";
     private Vector<Provider> providers;
     private static Providers instance;
+    private Provider defaultProvider;
+
+    public void setDefaultProvider(Provider provider) {
+        this.defaultProvider=provider;
+    }
+    public Provider getDefaultProvider() { return defaultProvider; }
 
     private Providers() {
         providers = new Vector<>();
-        providers.add(new Provider("193.205.230.23", 9998));
-        providers.add(new Provider("80.158.23.133", 9998));
-        providers.add(new Provider("54.72.110.23", 9998));
 
         instance = this;
     }
@@ -30,23 +36,40 @@ public class Providers {
         return instance;
     }
 
-    public Provider getBest() {
-        /*
+    public Provider getBest(int minRuntimeVersion) {
+
         Provider result=null;
         Vector<Provider> availables=new Vector<>();
         for (Provider provider:providers) {
             try {
                 provider.deviceQuery();
-                availables.add(provider);
+                if (provider.getRuntimeVersion()>=minRuntimeVersion) {
+                    CudaDeviceProp cudaDeviceProp = provider.getDeviceProperties();
+                    Log.d(LOG_TAG,"Name:"+cudaDeviceProp.getName());
+                    Log.d(LOG_TAG,"RuntimeVersion:"+provider.getRuntimeVersion());
+                    Log.d(LOG_TAG,"Minor:"+cudaDeviceProp.getMinor());
+                    Log.d(LOG_TAG,"Major:"+cudaDeviceProp.getMajor());
+
+                    boolean success = provider.matrixMul();
+                    if (success==true) {
+                        availables.add(provider);
+                    }
+                }
+
             } catch (IOException ex) {
-                throw new RuntimeException(ex);
+
             }
 
         }
-        result=availables.get(0);
+        if (availables.size()>0) {
+            result = availables.get(0);
+            for (Provider provider:availables) {
+                if (result.getWallClock()>provider.getWallClock()) {
+                    result=provider;
+                }
+            }
+        }
         return result;
-        */
-        return providers.get(0);
     }
 
     public void register(String host, int port) {
@@ -55,4 +78,8 @@ public class Providers {
     }
 
 
+
+    public void unregister() {
+        providers.removeAllElements();
+    }
 }
